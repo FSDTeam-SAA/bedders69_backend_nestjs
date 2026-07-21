04a47d67c4453a2848128ed9ac7ddc3c3cec2554
Nayem Sarkar <76911177+Nsarkar-XLR8@users.noreply.github.com>
2026-07-21 16:30:18 +0600

    Resolve merge conflict in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,12 +20,9 @@ import { ProductCategoryModule } from './app/module/product-category/product-cat
 import { ProductModule } from './app/module/product/product.module';
 import { CareModule } from './app/module/care/care.module';
 import { ProfileModule } from './app/module/profile/profile.module';
-<<<<<<< HEAD
 import { PackageModule } from './app/module/package/package.module';
 import { EntitlementModule } from './app/module/entitlement/entitlement.module';
-=======
 import { AgencyModule } from './app/module/agency/agency.module';
->>>>>>> 99463d40e1732479f324c110cf35e5e1c6987058
 
 @Module({
   imports: [
@@ -47,12 +44,9 @@ import { AgencyModule } from './app/module/agency/agency.module';
     ProductModule,
     CareModule,
     ProfileModule,
-<<<<<<< HEAD
     PackageModule,
     EntitlementModule,
-=======
     AgencyModule,
->>>>>>> 99463d40e1732479f324c110cf35e5e1c6987058
   ],
   controllers: [AppController],
   providers: [AppService],

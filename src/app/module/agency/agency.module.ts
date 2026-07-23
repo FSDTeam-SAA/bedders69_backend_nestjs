ec9c5fe27f8ab8ff1c670eda2740e0ad38167727
saurav11sarkar <sarkarsaurav11@outlook.com>
2026-07-23 14:00:06 +0600

    feat: enhance Agency module with file upload capabilities and profile management features

--- src/app/module/agency/agency.module.ts
+++ src/app/module/agency/agency.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../user/entities/user.entity';
 import { AgencyController } from './agency.controller';
 import { AgencyService } from './agency.service';
 import { Agency, AgencySchema } from './entities/agency.entity';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Agency.name, schema: AgencySchema }]),
+    MongooseModule.forFeature([
+      { name: Agency.name, schema: AgencySchema },
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
   controllers: [AgencyController],
   providers: [AgencyService],
--- src/app/module/agency/entities/agency.entity.ts
+++ src/app/module/agency/entities/agency.entity.ts
@@ -1,9 +1,13 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument, Types } from 'mongoose';
 
-export type agencyDocument = HydratedDocument<Agency>;
+export type AgencyDocument = HydratedDocument<Agency>;
+
 @Schema({ timestamps: true })
 export class Agency {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  userId!: Types.ObjectId;
+
   @Prop()
   name!: string;
 
@@ -23,7 +27,7 @@ export class Agency {
   discription!: string;
 
   @Prop()
-  alternatageEmail!: string;
+  alternatageEmail?: string;
 
   @Prop()
   address!: string;
@@ -32,7 +36,16 @@ export class Agency {
   specialisations!: string[];
 
   @Prop()
-  documents!: string[];
+  documents?: string[];
+
+  @Prop({ enum: ['approved', 'pending', 'rejected'], default: 'pending' })
+  status?: string;
+
+  @Prop({ enum: ['incomplete', 'complete'], default: 'incomplete' })
+  profileCompletionStatus!: string;
+
+  @Prop({ default: 0 })
+  profileCompletionPercentage!: number;
 }
 
 export const AgencySchema = SchemaFactory.createForClass(Agency);

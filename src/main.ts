ea35e78ba3c0dadf46bb30c5aa72720f17619f63
mehedishishir0 <mehedihasanshishir.info@gmail.com>
2026-09-06 14:54:28 +0600

    fix(server): increase express json and urlencoded body parser limit to 50mb

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,8 @@ async function bootstrap() {
   });
 
   app.use('/api/v1/webhook', express.raw({ type: 'application/json' }));
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
   app.use(cookieParser());
   app.enableCors({
